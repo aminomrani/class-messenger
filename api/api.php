62a0e04e147565bfa42f8c4dc19b15f445b1919c
aminomrani <aminf3344@gmail.com>
2022-03-09 19:59:22 +0330

    add jdf flie and fix login

--- api/api.php
+++ api/api.php
@@ -2,7 +2,7 @@
 #
 require_once 'config.php';
 require_once 'lib/Medoo.php';
-
+require_once 'lib/jdf.php';
 use database ; 
 $config = new database();
 use Medoo\Medoo;
@@ -18,6 +18,7 @@ $database = new Medoo([
 $username = $_GET['username'];
 $password = $_GET['password'];
 $step_type = $_GET['type'];
+$time = jdate('Y-m-d H:i:s');
 
 if(in_array($step_type,['gotroom','sendpm','login','register','getinfo']) == false){
     #make error message code : 502 for  false step_type
@@ -32,5 +33,12 @@ elseif($username == null or $password == null) {
 
 }
 elseif ($step_type == 'login') {
+    if ($database->select('users',['username'],[$username])[0] !== null){
+        if($database->select('login',['logintime'],[$username])[0] == null ){
+            $database->insert('users',['username'=>$username,'logintime'=>$time]);
+        }else{
+            $database->update('login',['logintime'=>$time],[$username]);
+        }
+    }
     
 }
